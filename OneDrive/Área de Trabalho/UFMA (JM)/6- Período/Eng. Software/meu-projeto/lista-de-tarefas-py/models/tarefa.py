1c57c4e218f23feaca98f05836d15e3c876ab4a8
Jeane Ferreira de Oliveira <jeane.2oliv.3@gmail.com>
2026-01-06 10:38:38 -0300

    organização-pastas-2

--- OneDrive/Área de Trabalho/UFMA (JM)/6- Período/Eng. Software/meu-projeto/lista-de-tarefas-py/models/tarefa.py
+++
@@ -1,8 +0,0 @@
-class Tarefa:
-    def __init__(self, nome, descricao, status):
-        self.nome = nome
-        self.descricao = descricao
-        self.status = status
-
-    def __str__(self):
-        return f"{self.nome} - {self.descricao} [{self.status.nome()}]"
